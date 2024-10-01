3a6647113bd652b458ede90c1eb49c28b491d878
Lukas Arts <lukas@betal112465.cs.uu.nl>
2024-10-01 23:26:33 +0200

    Removed some other new personal files

--- nnunetv2/paths.py
+++ nnunetv2/paths.py
@@ -18,9 +18,9 @@ import os
 PLEASE READ paths.md FOR INFORMATION TO HOW TO SET THIS UP
 """
 
-nnUNet_raw = "/home/lukas/UU/ASRA/Analysis/nnUNet/nnUNet_raw"
-nnUNet_preprocessed = "/home/lukas/UU/ASRA/Analysis/nnUNet/nnUNet_preprocessed"
-nnUNet_results = "/home/lukas/UU/ASRA/Analysis/nnUNet/nnUNet_results"
+nnUNet_raw = os.environ.get('nnUNet_raw')
+nnUNet_preprocessed = os.environ.get('nnUNet_preprocessed')
+nnUNet_results = os.environ.get('nnUNet_results')
 
 if nnUNet_raw is None:
     print("nnUNet_raw is not defined and nnU-Net can only be used on data for which preprocessed files "
