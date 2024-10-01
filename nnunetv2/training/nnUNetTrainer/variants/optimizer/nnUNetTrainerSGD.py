fd74afd09ca7174557055b94b1bd1b8255f0fc3c
Lukas Arts <lukas@betal112465.cs.uu.nl>
2024-10-01 23:23:03 +0200

    Added gridsearch.sh

---
+++ nnunetv2/training/nnUNetTrainer/variants/optimizer/nnUNetTrainerSGD.py
@@ -0,0 +1,19 @@
+import torch
+from torch.optim import Adam, AdamW
+
+from nnunetv2.training.lr_scheduler.polylr import PolyLRScheduler
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+
+
+class nnUNetTrainerSGD1e2(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.initial_lr = 1e-2
+
+
+class nnUNetTrainerSGD1e3(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.initial_lr = 1e-3
