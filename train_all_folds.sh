f15fc97efcef4b552221068eeb27bbc716fe3aec
Lukas Arts <lukas@betal112465.cs.uu.nl>
2024-10-15 14:56:55 +0200

    Removed files not belonging to .gitignore

--- train_all_folds.sh
+++
@@ -1,5 +0,0 @@
-nnUNetv2_train 13 1d 0 -p nnUNetResEncUNetMPlans -tr nnUNetTrainerAdam1en3
-nnUNetv2_train 13 1d 1 -p nnUNetResEncUNetMPlans -tr nnUNetTrainerAdam1en3
-nnUNetv2_train 13 1d 2 -p nnUNetResEncUNetMPlans -tr nnUNetTrainerAdam1en3
-nnUNetv2_train 13 1d 3 -p nnUNetResEncUNetMPlans -tr nnUNetTrainerAdam1en3
-nnUNetv2_train 13 1d 4 -p nnUNetResEncUNetMPlans -tr nnUNetTrainerAdam1en3
